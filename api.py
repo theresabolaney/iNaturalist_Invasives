8514dd913705ce9b3e40b7491d153c4273c70c62
Tom Smilack <tom@smilack.net>
2026-09-03 10:17:41 -0500

    Incorporate changes to API from api-json branch (PR #5)

--- api.py
+++ api.py
@@ -1,58 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 import json
 import requests
 
-# https://learning.postman.com/docs/design-apis/mock-apis/tutorials/mock-with-api
-# https://dev.to/terieyenike/creating-apis-with-flask-and-testing-in-postman-2ojn
-# https://docs.cloud.google.com/run/docs/quickstarts/build-and-deploy/deploy-python-service
-
-# Project setup:
-# https://flask.palletsprojects.com/en/stable/installation/
-# https://flask.palletsprojects.com/en/stable/quickstart/
-
 app = Flask(__name__)
 CORS(app)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 @app.get("/taxa/<int:id>")
 def get_taxon(id):
     iNaturalist_request = requests.get(f"https://api.inaturalist.org/v2/taxa/{id}?fields=name")
     iNaturalist_data = iNaturalist_request.json()
-    if "results" in iNaturalist_data and len(results := iNaturalist_data["results"]) > 0:
+
+    if "results" in iNaturalist_data and len(iNaturalist_data["results"]) > 0:
         # # Debugging: directly print iNaturalist response
         # return json.dumps(results)
 
-        responses = []
-        with open("dnr_regulated_species.json") as dnr_file:
-            dnr_data = json.load(dnr_file)
+        taxon = iNaturalist_data["results"][0]
+        dnr_matches = filter_by_scientific_name(taxon)
 
-            for result in results:
-                if "id" in result and "name" in result:
-                    responses.append(f"iNaturalist returned {result["id"]} = {result["name"]}")
-                    # TODO: also get common name from iNaturalist
-                    dnr_matches = []
-                    for regulated_species in dnr_data:
-                        for name in regulated_species["scientificNames"]:
-                            if result["name"] in name or name in result["name"]:
-                                dnr_matches.append(regulated_species)
-                                break
+        response = dict(
+            taxonId=taxon["id"],
+            total_results=len(dnr_matches),
+            results=dnr_matches
+        )
 
-                    if len(dnr_matches) > 0:
-                        for dnr_match in dnr_matches:
-                            responses.append(
-                                             f" * Found match in DNR list:\n"
-                                             f"   * Common names: {", ".join(dnr_match["commonNames"])}\n"
-                                             f"   * Scientific names: {", ".join(dnr_match["scientificNames"])}\n"
-                                             f"   * Status: {dnr_match["regulated"]}"
-                                         )
-                    else:
-                        responses.append(" * Did not find match in DNR list")
-
-        return "<pre>" + "\n".join(responses) + "</pre>"
+        return jsonify(response)
 
     else:
-        return f"iNaturalist returned no results for {id}"
+        return f"Error 404: taxon {id} not found", 404
+
+
+def names_match(a, b):
+    return a in b or b in a
+
+def is_name_of(name):
+    return lambda dnr_species: (
+        sum(1 for dnr_name in dnr_species["scientificNames"]
+            if names_match(name, dnr_name))
+        > 0
+    )
+
+def filter_by_scientific_name(taxon):
+    if "id" not in taxon or "name" not in taxon:
+        return []
+
+    with open("dnr_regulated_species.json") as dnr_file:
+        dnr_data = json.load(dnr_file)
+        return list(filter(is_name_of(taxon["name"]), dnr_data))
